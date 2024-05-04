503074be6361b5d07e8b8b42de7821dfedba3e16
freddiefreeloader1 <emregursoy9@gmail.com>
2024-05-04 18:27:20 +0200

    changed input cost

--- Manipulator-master/objective.jl
+++ Manipulator-master/objective.jl
@@ -1,52 +1,31 @@
 struct InputCost
-    weight1::Float64
-    weight2::Float64
-    idx::Int
-
-    function InputCost(idx, weight1=1.0, weight2=1.0)
-        new(idx, weight1, weight2)
-    end
+    value::Float64
+    gradient_x::Vector{Float64}
+    hessian_x::Matrix{Float64}
+    gradient_u::Vector{Float64}
+    hessian_u::Matrix{Float64}
+    R::Matrix{Float64}
 end
 
-function evaluate(ic::InputCost, x, u)
-    return ic.weight1 * u[1]^2 + ic.weight2 * u[2]^2
+function evaluate(ic::InputCost, u)
+    ic.value = 0.5*(u' * ic.R * u)
 end
 
 function gradient_x(ic::InputCost, x, u)
-    return zeros(length(x))
+    ic.gradient_x = zeros(length(x))
 end
 
 function gradient_u(ic::InputCost, x, u)
-    grad_u = zeros(length(u))
-    grad_u[1] = 2 * ic.weight1 * u[1]
-    grad_u[2] = 2 * ic.weight2 * u[2]
-    return grad_u
+    ic.gradient_u = ic.R * u
 end
 
 function hessian_x(ic::InputCost, x, u)
-    return zeros(length(x), length(x))
+   ic.hessian_x = zeros(length(x), length(x))
 end
 
 function hessian_u(ic::InputCost, x, u)
-    hess_u = zeros(length(u), length(u))
-    hess_u[1, 1] = 2 * ic.weight1
-    hess_u[2, 2] = 2 * ic.weight2
-    return hess_u
+    ic.hessian_u = ic.R
 end
-
-
-# make a trial
-
-ic = InputCost(1, 2, 1)
-x = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-u = [3.0, 4.0]
-
-println(evaluate(ic, x, u))
-println(gradient_x(ic, x, u))
-println(gradient_u(ic, x, u))
-println(hessian_x(ic, x, u))
-println(hessian_u(ic, x, u))
-
 
 mutable struct referenceCost
     value::Float64
@@ -62,7 +41,10 @@ function gradient_x(rc::referenceCost, x, Q)
     rc.gradient = Q * x
 end
 
-function hesian_x(rc::referenceCost, x, Q)
+function hessian_x(rc::referenceCost, x, Q)
     rc.hessian = Q
 end
+
+
+
 
