36c73171a9a29a4544e56520d1a0394040a38daf
freddiefreeloader1 <emregursoy9@gmail.com>
2024-05-18 18:58:35 +0200

    last config of plotting

--- Manipulator-master/plotting.jl
+++ Manipulator-master/plotting.jl
@@ -1,13 +1,22 @@
 using Plots
 
 # Function to calculate the positions of the link ends with absolute angles
-function forward_kinematics_absolute(theta1, theta2, l1, l2)
+function forward_kinematics_absolute_1(theta1, theta2, l1, l2)
     x1 = l1 * cos(theta1)
     y1 = l1 * sin(theta1)
     x2 = x1 + l2 * cos(theta2)
     y2 = y1 + l2 * sin(theta2)
     return (x1, y1, x2, y2)
 end
+
+function forward_kinematics_absolute_2(theta1, theta2, l1, l2)
+    x1 = l1 * cos(theta1)
+    y1 = -l1 * sin(theta1)
+    x2 = x1 + l2 * cos(theta2)
+    y2 = y1 - l2 * sin(theta2)
+    return (x1, y1, x2, y2)
+end
+
 
 # Function to get the corners of the rectangle representing the link
 function get_rectangle_corners(x0, y0, x1, y1, width)
@@ -30,7 +39,7 @@ function get_jaw_corners(x, y, theta, jaw_length, jaw_width)
 end
 
 # Animation function
-function animate_robots(theta1_traj, theta2_traj, theta3_traj, theta4_traj, d, l1=1.0, l2=1.0, width=0.1, jaw_length=0.2, jaw_width=0.05)
+function animate_robots(theta1_traj, theta2_traj, theta3_traj, theta4_traj, d, l1=1.0, l2=1.0, l3= 1.0, l4 = 1.0, width=0.1, jaw_length=0.2, jaw_width=0.05)
     n_frames = length(theta1_traj)  # Number of frames in the animation
     
     # Store the tip positions for the trajectory
@@ -47,10 +56,10 @@ function animate_robots(theta1_traj, theta2_traj, theta3_traj, theta4_traj, d, l
         theta4 = theta4_traj[i]
         
         # Forward kinematics for robot 1 with absolute angles
-        x1, y1, x2, y2 = forward_kinematics_absolute(theta1, theta2, l1, l2)
+        x1, y1, x2, y2 = forward_kinematics_absolute_1(theta1, theta2, l1, l2)
         
         # Forward kinematics for robot 2 with absolute angles (base shifted by d in y direction)
-        x3, y3, x4, y4 = forward_kinematics_absolute(theta3, theta4, l1, l2)
+        x3, y3, x4, y4 = forward_kinematics_absolute_2(theta3, theta4, l3, l4)
         y3 += d
         y4 += d
         
@@ -82,10 +91,10 @@ function animate_robots(theta1_traj, theta2_traj, theta3_traj, theta4_traj, d, l
         plot!(p, [jaw2_corners2[1][1], jaw2_corners2[2][1]], [jaw2_corners2[1][2], jaw2_corners2[2][2]], color=:black, label="")
 
         # Plot the tips' trajectories
-        scatter!(p, [pos[1] for pos in tip_positions1], [pos[2] for pos in tip_positions1], color=:black, label="Tip 1 Trajectory")
-        scatter!(p, [pos[1] for pos in tip_positions2], [pos[2] for pos in tip_positions2], color=:purple, label="Tip 2 Trajectory")
-        scatter!(p, [pos[1] for pos in tip_positions3], [pos[2] for pos in tip_positions3], color=:yellow, label="Tip 3 Trajectory")
-        scatter!(p, [pos[1] for pos in tip_positions4], [pos[2] for pos in tip_positions4], color=:cyan, label="Tip 4 Trajectory")
+        scatter!(p, [pos[1] for pos in tip_positions1], [pos[2] for pos in tip_positions1], color=:black, label="Tip 1 Trajectory", markersize = 1)
+        scatter!(p, [pos[1] for pos in tip_positions2], [pos[2] for pos in tip_positions2], color=:purple, label="Tip 2 Trajectory", markersize = 1)
+        scatter!(p, [pos[1] for pos in tip_positions3], [pos[2] for pos in tip_positions3], color=:yellow, label="Tip 3 Trajectory", markersize = 1)
+        scatter!(p, [pos[1] for pos in tip_positions4], [pos[2] for pos in tip_positions4], color=:cyan, label="Tip 4 Trajectory", markersize = 1)
 
         display(p)
     end every 2
