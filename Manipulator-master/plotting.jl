74fe1232459755084212eba41596c8c2061d7294
freddiefreeloader1 <emregursoy9@gmail.com>
2024-05-18 18:27:38 +0200

    added plots

--- Manipulator-master/plotting.jl
+++ Manipulator-master/plotting.jl
@@ -1,23 +1,35 @@
 using Plots
 
-@userplot CirclePlot
-@recipe function f(cp::CirclePlot)
-    x, y, i = cp.args
-    n = length(x)
-    inds = circshift(1:n, 1 - i)
-    linewidth --> range(0, 10, length = n)
-    seriesalpha --> range(0, 1, length = n)
-    aspect_ratio --> 1
-    label --> false
-    x[inds], y[inds]
+# Function to calculate the positions of the links
+function forward_kinematics(theta1, theta2, l1, l2)
+    x1 = l1 * cos(theta1)
+    y1 = l1 * sin(theta1)
+    x2 = x1 + l2 * cos(theta1 + theta2)
+    y2 = y1 + l2 * sin(theta1 + theta2)
+    return (x1, y1, x2, y2)
 end
 
-n = 150
-t = range(0, 2π, length = n)
-x = sin.(t)
-y = cos.(t)
+# Parameters
+l1 = 1.0  # Length of the first link
+l2 = 1.0  # Length of the second link
+n_frames = 100  # Number of frames in the animation
 
-anim = @animate for i ∈ 1:n
-    circleplot(x, y, i)
-end
-gif(anim, "anim_fps15.gif", fps = 15)
+# Define the range of joint angles for the animation
+theta1_range = range(0, stop=2π, length=n_frames)
+theta2_range = range(0, stop=2π, length=n_frames)
+
+# Create a plot object
+p = plot(xlim=(-2, 2), ylim=(-2, 2), legend=false)
+
+# Animation loop
+@gif for i in 1:n_frames
+    theta1 = theta1_range[i]
+    theta2 = theta2_range[i]
+    x1, y1, x2, y2 = forward_kinematics(theta1, theta2, l1, l2)
+    
+    plot!(p, [0, x1], [0, y1], label="Link 1", lw=3, color=:blue)
+    plot!(p, [x1, x2], [y1, y2], label="Link 2", lw=3, color=:red)
+    
+    scatter!([0, x1, x2], [0, y1, y2], color=:black)
+end every 2
+
